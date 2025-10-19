41581b839f6d7430f161a53e285d8df8c836ff69
Alexandra Mo <ymo@sh03-04n21.int>
2025-10-18 18:03:16 -0700

    add parallelism for making gene plots

--- cNMF_benchmarking_pipeline/Plotting/Slurm_Version/cNMF_perturbed_gene_analysis.py
+++ cNMF_benchmarking_pipeline/Plotting/Slurm_Version/cNMF_perturbed_gene_analysis.py
@@ -3,15 +3,18 @@ import muon as mu
 import numpy as np
 import pandas as pd
 import argparse
+import yaml
+import os
 
 # Change path to wherever you have repo locally
 sys.path.append('/oak/stanford/groups/engreitz/Users/ymo/Tools/cNMF_benchmarking/cNMF_benchmarking_pipeline')
 
 from Plotting.src import plot_umap_per_gene, plot_top_program_per_gene, perturbed_gene_dotplot,\
-                                      plot_log2FC, plot_volcano, programs_dotplot, analyze_correlations, \
-                                      create_comprehensive_plot, create_gene_correlation_waterfall, convert_with_mygene, \
-                                      convert_adata_with_mygene, read_npz, merge_svgs_to_pdf, merge_pdfs_in_folder
-
+                         plot_log2FC, plot_volcano, programs_dotplot, analyze_correlations, \
+                         create_gene_correlation_waterfall, \
+                         convert_with_mygene, convert_adata_with_mygene, read_npz, \
+                         merge_pdfs_in_folder, merge_svgs_to_pdf, create_comprehensive_plot, rename_adata_gene_dictionary, \
+                         rename_list_gene_dictionary, plot_umap_per_gene_guide, process_single_gene, parallel_gene_processing
 
 
 if __name__ == '__main__':
@@ -19,67 +22,57 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--mdata_path', type=str, required=True)  
-    parser.add_argument('--gene_loading_path', type=str, required=True)
-    parser.add_argument('--program_loading_path', type=str, required=True)
     parser.add_argument('--perturb_path', type=str, required=True) # partial path for each day
     parser.add_argument('--top_program',  type=int, default=5)
     parser.add_argument('--p_value',  type=float, default=0.05)
     parser.add_argument('--pdf_save_path',  type=str, required=True)
     parser.add_argument('--PDF',  action="store_true")  
+    parser.add_argument('--file_to_dictionary',  type=str, default = None)  
 
 
 
     args = parser.parse_args()
 
 
+    # save comfigs used         
+    args_dict = vars(args)
+    with open(f'{args.pdf_save_path}/config.yml', 'w') as f:
+        yaml.dump(args_dict, f, default_flow_style=False, width=1000)
+        
+
     #read mdata
     mdata = mu.read_h5mu(args.mdata_path)
 
-    # replace name
-    replacement_dict = {
-        'sample_D3': 'D3',
-        'sample_D2': 'D2', 
-        'sample_D1': 'D1'
-    }  
-
-    mdata['rna'].obs['sample'] = mdata['rna'].obs['sample'].replace(replacement_dict)
-
-    perturbed_gene = np.unique(mdata['cNMF'].uns["guide_targets"])
-
     # found detected perturbed gene
-    gene_list = convert_with_mygene(pd.DataFrame(index=mdata.var_names.tolist())) # convert gene id to geene name
-    perturbed_gene_found = list(set(gene_list.index) & set(perturbed_gene))
+    perturbed_gene = np.unique(mdata['cNMF'].uns["guide_targets"])
+    gene_list = rename_list_gene_dictionary(mdata['rna'].var_names.tolist(), args.file_to_dictionary) # convert gene id to geene name
+    perturbed_gene_found = list(set(gene_list) & set(perturbed_gene.tolist()))
 
     # sort list by alphabetical order 
     perturbed_gene_found = sorted(perturbed_gene_found)
-
     print(f"there are {len(perturbed_gene_found)} perturbed gene found")
-
-
-    # Graph all pdf 
-    for gene in perturbed_gene_found:
-
-        create_comprehensive_plot(
-            mdata,
-            args.perturb_path,
-            args.gene_loading_path,
-            args.program_loading_path,
-            gene,
-            save_path=args.pdf_save_path,
-            save_name=f"{gene}",
-            figsize=(35, 35),
+    
+    print("Starting parallel gene processing...")                                                         
+    try: 
+        # Graph all pdf 
+        result = parallel_gene_processing( 
+            mdata = mdata,
+            perturb_path = args.perturb_path,
+            perturbed_gene_list = perturbed_gene_found,
+            file_to_dictionary = args.file_to_dictionary,
+            save_path = args.pdf_save_path,
+            figsize = (30, 30),
             show=False,
-            PDF = args.PDF
+            PDF=args.PDF,
+            n_processes = -1
         )
-
+        print(f"Parallel processing completed successfully. Results: {len(result) if result else 'None'}")                                                                                                          
+    except Exception as e:                                                                                 
+        print(f"ERROR in parallel_gene_processing: {e}")  
+    
     # merge pdf 
     if args.PDF:
         merge_pdfs_in_folder(args.pdf_save_path)
     else:
         merge_svgs_to_pdf(args.pdf_save_path)
 
-    # save comfigs used         
-    args_dict = vars(args)
-    with open(f'{args.pdf_save_path}/Plot/config.yml', 'w') as f:
-        yaml.dump(args_dict, f, default_flow_style=False, width=1000)
-        
--- cNMF_benchmarking_pipeline/Plotting/Slurm_Version/cNMF_perturbed_gene_analysis.sh
+++ cNMF_benchmarking_pipeline/Plotting/Slurm_Version/cNMF_perturbed_gene_analysis.sh
@@ -7,9 +7,8 @@
 #SBATCH --partition=engreitz            # partition name
 #SBATCH --time=5:00:00                  # Time limit 
 #SBATCH --nodes=1                       # Number of nodes
-#SBATCH --ntasks=1                      # Number of tasks
-#SBATCH --cpus-per-task=1               # CPUs per task
-#SBATCH --mem=64G                       # Memory per node
+#SBATCH --cpus-per-task=20               # CPUs per task
+#SBATCH --mem=96G                       # Memory per node
 
 # Email notifications
 #SBATCH --mail-type=BEGIN              # Send email when job starts
@@ -50,14 +49,13 @@ echo "Python path: $(which python)"
 # Run the Python script
 echo "Running Python script..."
 python3 /oak/stanford/groups/engreitz/Users/ymo/Tools/cNMF_benchmarking/cNMF_benchmarking_pipeline/Plotting/Slurm_Version/cNMF_perturbed_gene_analysis.py\
-        --mdata_path "/oak/stanford/groups/engreitz/Users/ymo/NMF_re-inplementing/Script/Revant_code/original_code/analysis/240816_clustering_annotation_package/cNMF_100_0.2_preprocessed.h5mu" \
-        --gene_loading_path "/oak/stanford/groups/engreitz/Users/ymo/NMF_re-inplementing/Results/cNMF_100_07102024/consensus_NMF/consensus_NMF.gene_spectra_score.k_100.dt_0_2.txt"\
-        --program_loading_path "/oak/stanford/groups/engreitz/Users/ymo/NMF_re-inplementing/Script/Revant_code/original_code/analysis/240701_cNMF/output/all/consensus_NMF/consensus_NMF.usages.k_100.dt_0_2.consensus.txt"\
-        --perturb_path "/oak/stanford/groups/engreitz/Users/ymo/NMF_re-inplementing/Script/Revant_code/original_code/shared/250110_ipsc_ec_dashboard_setup/cNMF_100/cNMF_100_gene_sample"\
-        --top_program 5\
+        --mdata_path "/oak/stanford/groups/engreitz/Users/ymo/NMF_re-inplementing/Cell_data/Tri_orginal_data.h5mu" \
+        --perturb_path "/oak/stanford/groups/engreitz/Users/ymo/NMF_re-inplementing/Script/Revant_code/orginal_code/shared/250110_ipsc_ec_dashboard_setup/cNMF_100/cNMF_100_gene_sample"\
+        --top_program 5 \
         --p_value 0.05 \
-        --pdf_save_path "$LOG_DIR"\
-        --PDF
+        --pdf_save_path "$LOG_DIR" \
+        --PDF \
+        --file_to_dictionary "/oak/stanford/groups/engreitz/Users/ymo/Tools/cNMF_benchmarking/cNMF_benchmarking_pipeline/Evaluation/Resources/weissman_guides_with_coordinates.tsv"
 
 
 
