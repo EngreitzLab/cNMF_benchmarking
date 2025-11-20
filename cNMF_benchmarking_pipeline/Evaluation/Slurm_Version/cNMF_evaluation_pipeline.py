ace7fc2e8e0ac35c091cbda4b520d0bb97f4c4e7
Alexandra Mo <ymo@sh04-02n16.int>
2025-11-20 13:17:43 -0800

    update evaluation with formatting echeck

--- cNMF_benchmarking_pipeline/Evaluation/Slurm_Version/cNMF_evaluation_pipeline.py
+++ cNMF_benchmarking_pipeline/Evaluation/Slurm_Version/cNMF_evaluation_pipeline.py
@@ -3,7 +3,7 @@ import sys
 import yaml
 import logging
 import mudata
-import muon
+import muon as mu
 import pandas as pd
 import numpy as np
 import argparse
@@ -23,49 +23,30 @@ from Evaluation.src import (
     compute_explained_variance,
     compute_motif_enrichment
 )
-from Evaluation.src.enrichment_trait import process_enrichment_data
-
-def _assign_guide(mdata, file, key = "rna"):
-
-    # read mdata with guide
-    mdata_guide = muon.read(file)
-
-    mdata['cNMF'].uns["guide_names"] = mdata_guide[key].uns["guide_names"]
-    mdata['cNMF'].uns["guide_targets"] = mdata_guide[key].uns["guide_targets"]
-    mdata['cNMF'].obsm["guide_assignment"] = mdata_guide[key].obsm["guide_assignment"]
 
 
-def rename_gene(mdata):
+from Evaluation.src import (
+    rename_gene_mygene,
+    rename_gene_dictionary,
+    check_evaluation_pipeline_format,
+    check_gene_names
+)
 
-    mg = mygene.MyGeneInfo()
-    gene_list = mdata['cNMF'].uns['var_names'].tolist()
-    annotations = mg.querymany(gene_list, scopes='ensembl.gene', fields='symbol', species='human')
-
-    # Process the results to create mapping
-    gene_dict = {}
-    
-    for item in annotations:
-        if 'symbol' in item:
-            gene_dict[item['query']] = item['symbol']
-
-    mdata['cNMF'].uns['var_names'] = [gene_dict.get(x, x) for x in mdata['cNMF'].uns['var_names']]
-
-def rename_gene_dictionary(mdata, dictionary_file_path):
-
-    # Convert mapping result to list before assignment
-    df = pd.read_csv(dictionary_file_path, sep='\t')
-    ensemble_to_gene = dict(zip(df['ensembl_id'], df['gene']))
-    new_names = [ensemble_to_gene.get(x, x) for x in mdata['rna'].var_names]
-    mdata['rna'].var_names = (new_names)
+def _assign_guide(mdata, mdata_guide):
+        mdata['rna'].var_names = mdata['rna'].var['symbol']
+        mdata['cNMF'].uns['guide_names'] = mdata_guide['guide'].var['guide_id']
+        mdata['cNMF'].uns['guide_targets'] = mdata_guide['guide'].var['intended_target_name']
+        mdata['cNMF'].obsm['guide_assignment'] = mdata_guide['guide'].layers['guide_assignment'].toarray()
 
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--input_folder', type=str, required=True)  
-    parser.add_argument('--mdata_guide', type=str, default='adata')
+    #IO info
+    parser.add_argument('--out_dir', help='path to make cnmf object', type=str, required=True)  
+    parser.add_argument('--run_name', help='name of cnmf obj',type=str, required=True)  
     parser.add_argument('--K', nargs='*', type=int, default=None) # allow zero input 
-    parser.add_argument('--thre', default = '2.0')
+    parser.add_argument('--sel_thresh', nargs='*', type=int, default=None) # allow zero input 
 
     # running different tests
     parser.add_argument('--Perform_categorical', action="store_true")
@@ -75,13 +56,18 @@ if __name__ == '__main__':
     parser.add_argument('--Perform_explained_variance', action="store_true")
     parser.add_argument('--Perform_motif', action="store_true")
 
-    parser.add_argument('--X_normalized', type=str,  help='path to normalized input cell x gene matrix from cNMF pipeline', required=True)
-    parser.add_argument('--out_dir', help='path to make cnmf object', type=str, required=True)  
-    parser.add_argument('--run_name', help='name of cnmf obj',type=str, required=True)  
-    parser.add_argument('--assign_guide', action="store_true")
-    parser.add_argument('--assign_guide_key', type = str, default = "rna")
-    parser.add_argument('--dictionary_file_path', type = str, default= None)
+    # resourses 
+    parser.add_argument('--X_normalized_path', type=str,  help='path to normalized input cell x gene matrix from cNMF pipeline', required=True)
+    parser.add_argument('--mdata_guide_path', type=str,  help='path to mdata with correct guide assignments', required=True)
+    parser.add_argument('--guide_annotation_path', type=str,  help='path to file with guide informations', required=True)
+    parser.add_argument('--gwas_data_path', type=str,  help='path to file with gwas information for trait enrichment test', required=True)
+    parser.add_argument('--reference_gtf_path', type=str,  help='path to reference GTF file for checking gene names')
 
+    # keys
+    parser.add_argument('--data_key', help='access gene expression in mdata',type=str, default="rna") 
+    parser.add_argument('--prog_key', help='access cNMF program in mdata',type=str,  default="cNMF") 
+    parser.add_argument('--categorical_key', help='access cell condition in obs',type=str, default="sample")  
+    parser.add_argument('--organism', help='data species',type=str, default="human")  
 
 
     args = parser.parse_args()
@@ -89,119 +75,141 @@ if __name__ == '__main__':
 
     # either change the array here or run each component in parallel
     if args.K is None:
-        k_value = [30, 50, 60, 80, 100, 200, 250, 300]
-    else:
-        k_value = args.K
+        args.K = [30, 50, 60, 80, 100, 200, 250, 300]
+
+    if args.sel_thresh is None:
+        args.sel_thresh = [0.4, 0.8, 2.0]
+
+
+    # create output directory       
+    os.makedirs((f'{args.out_dir}/{args.run_name}/Eval'), exist_ok=True)
+
+    # save comfigs used         
+    args_dict = vars(args)
+    job_id = os.environ.get('SLURM_JOB_ID')
+    with open(f'{args.out_dir}/{args.run_name}/Eval/config_{job_id}.yml', 'w') as f:
+        yaml.dump(args_dict, f, default_flow_style=False, width=1000)
+
 
     # defind objects used in explained variance
     cnmf_obj = cnmf.cNMF(output_dir=args.out_dir, name=args.run_name)
-    X_norm = sc.read_h5ad(args.X_normalized)
+    X_norm = sc.read_h5ad(args.X_normalized_path)
     X = X_norm.X
 
+    # list of non-targeting guides
+    df_target = pd.read_csv(args.guide_annotation_path, sep = "\t", index_col = 0)
+    df_target_non = df_target[df_target["targeting"] == False]
+    reference_targets = df_target_non.index.values.tolist()
 
-    for k in k_value:  
+    # read guide
+    mdata_guide = mu.read(args.mdata_guide_path)
 
-        os.makedirs(f"{args.input_folder}/Eval/{k}", exist_ok=True)
-        output_folder = f"{args.input_folder}/Eval/{k}"
+    for sel_thresh in args.sel_thresh:
+        for k in args.K:  
+            
+            output_folder = f"{args.out_dir}/{args.run_name}/Eval/{k}_{str(sel_thresh).replace('.','_')}"
 
-        # Load mdata
-        mdata = mudata.read('{}/adata/cNMF_{}_{}.h5mu'.format(args.input_folder,k,args.thre))
+            os.makedirs(output_folder, exist_ok=True)
 
+            # Load mdata
+            mdata = mu.read('{out_dir}/{run_name}/adata/cNMF_{k}_{sel_thresh}.h5mu'.format(out_dir = args.out_dir,
+                                                                                    run_name =args.run_name,
+                                                                                    k=k,
+                                                                                    sel_thresh = str(sel_thresh).replace('.','_'))) 
+            # assign guide
+            _assign_guide(mdata, mdata_guide)   
 
-        # Run categorical assocation
-        if args.Perform_categorical: 
-            results_df, posthoc_df = compute_categorical_association(mdata, prog_key='cNMF', categorical_key='sample', 
-                                                                    pseudobulk_key=None, test='dunn', n_jobs=-1, inplace=False)
+            # checks for correct mdata format
+            if not check_evaluation_pipeline_format(mdata,prog_key=args.prog_key):
+                raise ValueError("mdata format is incorrect")
 
-            results_df.to_csv('{}/{}_categorical_association_results.txt'.format(output_folder,k), sep='\t', index=False) # This was made wide form to insert into .var of the program anndata.
-            posthoc_df.to_csv('{}/{}_categorical_association_posthoc.txt'.format(output_folder,k), sep='\t', index=False)
-
-
-        # Run perturbation assocation
-        if args.Perform_perturbation: 
-
-            if args.assign_guide: _assign_guide(mdata, args.mdata_guide, args.assign_guide_key) 
-
-            for samp in mdata['rna'].obs['sample'].unique():
-                mdata_ = mdata[mdata['rna'].obs['sample']==samp]
-                test_stats_df = compute_perturbation_association(mdata_, prog_key='cNMF', 
-                                                                collapse_targets=True,
-                                                                pseudobulk=False,
-                                                                reference_targets=('non-targeting'),
-                                                                n_jobs=-1, inplace=False)
-
-                test_stats_df.to_csv('{}/{}_perturbation_association_results_{}.txt'.format(output_folder,k,samp), sep='\t', index=False)
-    
-
-        # Gene-set enrichment
-        if args.Perform_geneset:
-            pre_res = compute_geneset_enrichment(mdata, prog_key='cNMF', data_key='rna', prog_nam=None,
-                                                organism='human', library='Reactome_2022', method="fisher",
-                                                database='enrichr', loading_rank_thresh=300, n_jobs=-1, 
-                                                inplace=False, user_geneset=None)
-            pre_res.to_csv('{}/{}_geneset_enrichment.txt'.format(output_folder,k), sep='\t', index=False)
+            valid = check_gene_names(mdata,prog_key=args.prog_key, data_key=args.data_key,categorical_key=args.categorical_key,reference_gtf_path=args.reference_gtf_path)
+            if not  valid["is_valid"]:
+                raise ValueError("mdata gene naming is incorrect")
 
 
-            # GO Term enrichment
-            pre_res = compute_geneset_enrichment(mdata, prog_key='cNMF', data_key='rna', prog_nam=None,
-                                                organism='human', library='GO_Biological_Process_2023', method="fisher",
-                                                database='enrichr', loading_rank_thresh=300, n_jobs=-1, 
-                                                inplace=False, user_geneset=None)
-            pre_res.to_csv('{}/{}_GO_term_enrichment.txt'.format(output_folder,k), sep='\t', index=False)
+            # Run categorical assocation
+            if args.Perform_categorical: 
+                results_df, posthoc_df = compute_categorical_association(mdata, prog_key='cNMF', categorical_key=args.categorical_key, 
+                                                                        pseudobulk_key=None, test='dunn', n_jobs=-1, inplace=False)
+
+                results_df.to_csv('{}/{}_categorical_association_results.txt'.format(output_folder,k), sep='\t', index=False) # This was made wide form to insert into .var of the program anndata.
+                posthoc_df.to_csv('{}/{}_categorical_association_posthoc.txt'.format(output_folder,k), sep='\t', index=False)
 
 
-        # Run trait enrichment
-        if args.Perform_trait:
-            pre_res_trait = compute_trait_enrichment(mdata, gwas_data='/oak/stanford/groups/engreitz/Users/ymo/Tools/cNMF_benchmarking/cNMF_benchmarking_pipeline/Evaluation/Resources/OpenTargets_L2G_Filtered.csv.gz', 
-                                                    prog_key='cNMF', prog_nam=None, data_key='rna', 
-                                                    library='OT_GWAS', n_jobs=-1, inplace=False, 
-                                                    key_column='trait_efos', gene_column='gene_name', 
-                                                    method='fisher', loading_rank_thresh=300)
-            pre_res_trait.to_csv('{}/{}_trait_enrichment.txt'.format(output_folder,k), sep='\t', index=False)
+            # Run perturbation assocation
+            if args.Perform_perturbation: 
+                for samp in mdata['rna'].obs[args.categorical_key].unique():
+                    mdata_ = mdata[mdata['rna'].obs[args.categorical_key]==samp]
+                    test_stats_df = compute_perturbation_association(mdata_, prog_key=args.prog_key, 
+                                                                    collapse_targets=True,
+                                                                    pseudobulk=False,
+                                                                    reference_targets=reference_targets,
+                                                                    n_jobs=-1, inplace=False)
+
+                    test_stats_df.to_csv('{}/{}_perturbation_association_results_{}.txt'.format(output_folder,k,samp), sep='\t', index=False)
+        
+
+            # Gene-set enrichment
+            if args.Perform_geneset:
+                pre_res = compute_geneset_enrichment(mdata, prog_key=args.prog_key, data_key=args.data_key, prog_nam=None,
+                                                    organism=args.organism, library='Reactome_2022', method="fisher",
+                                                    database='enrichr', loading_rank_thresh=300, n_jobs=-1, 
+                                                    inplace=False, user_geneset=None)
+                pre_res.to_csv('{}/{}_geneset_enrichment.txt'.format(output_folder,k), sep='\t', index=False)
 
 
-        # Run motif analysis 
-        if args.Perform_motif:
-
-            fimo_thresh_enhancer = 1e-6
-            fimo_thresh_promoter = 1e-4
-
-            if args.dictionary_file_path is None:
-                rename_gene(mdata) # rename the gene from Ensembl ID to gene symbols using mygene
-            else:
-                rename_gene_dictionary(mdata, args.dictionary_file_path)
-
-            for i in range(4):
-                for class_, thresh in [('enhancer', fimo_thresh_enhancer), 
-                                    ('promoter', fimo_thresh_promoter)]:
-
-                    loci_file = '/oak/stanford/groups/engreitz/Users/ymo/Tools/cNMF_benchmarking/cNMF_benchmarking_pipeline/Evaluation/Resources/scE2G_links/EnhancerPredictionsAllPutative.ForVariantOverlap.shrunk150bp_D{}_{}.tsv'.format(i, class_)
-                    motif_match_df, motif_count_df, motif_enrichment_df = compute_motif_enrichment(
-                        mdata, 
-                        prog_key='cNMF',
-                        data_key='rna',
-                        motif_file='/oak/stanford/groups/engreitz/Users/ymo/Tools/cNMF_benchmarking/cNMF_benchmarking_pipeline/Evaluation/Resources/hocomoco_meme.meme',
-                        seq_file='/oak/stanford/groups/engreitz/Users/ymo/Tools/cNMF_benchmarking/cNMF_benchmarking_pipeline/Evaluation/Resources/hg38.fa',
-                        loci_file=loci_file,
-                        window=1000,
-                        sig=thresh,
-                        eps=1e-4,
-                        n_top=2000,
-                        n_jobs=-1,
-                        inplace=False
-                    )
-
-                    motif_match_df.to_csv(os.path.join(output_folder, f'cNMF_{class_}_pearson_topn2000_sample_D{i}_motif_match.txt'), sep='\t', index=False)
-                    motif_count_df.to_csv(os.path.join(output_folder, f'cNMF_{class_}_pearson_topn2000_sample_D{i}_motif_count.txt'), sep='\t', index=False)
-                    motif_enrichment_df.to_csv(os.path.join(output_folder, f'cNMF_{class_}_pearson_topn2000_sample_D{i}_motif_enrichment.txt'), sep='\t', index=False)
+                # GO Term enrichment
+                pre_res = compute_geneset_enrichment(mdata, prog_key=args.prog_key, data_key=args.data_key, prog_nam=None,
+                                                    organism=args.organism, library='GO_Biological_Process_2023', method="fisher",
+                                                    database='enrichr', loading_rank_thresh=300, n_jobs=-1, 
+                                                    inplace=False, user_geneset=None)
+                pre_res.to_csv('{}/{}_GO_term_enrichment.txt'.format(output_folder,k), sep='\t', index=False)
 
 
-        # Run explained variance
-        if args.Perform_explained_variance:
-            compute_explained_variance(cnmf_obj, X, k, output_folder = output_folder)
+            # Run trait enrichment
+            if args.Perform_trait:
+                pre_res_trait = compute_trait_enrichment(mdata, gwas_data=args.gwas_data_path, 
+                                                        prog_key=args.prog_key, prog_nam=None, data_key=args.data_key, 
+                                                        library='OT_GWAS', n_jobs=-1, inplace=False, 
+                                                        key_column='trait_efos', gene_column='gene_name', 
+                                                        method='fisher', loading_rank_thresh=300)
+                pre_res_trait.to_csv('{}/{}_trait_enrichment.txt'.format(output_folder,k), sep='\t', index=False)
 
-    
-    # save comfigs used         
-    args_dict = vars(args)
-    with open(f'{args.input_folder}/Eval/config.yml', 'w') as f:
-        yaml.dump(args_dict, f, default_flow_style=False, width=1000)
+
+            # Run motif analysis 
+            if args.Perform_motif:
+
+                fimo_thresh_enhancer = 1e-6
+                fimo_thresh_promoter = 1e-4
+
+                for samp in mdata['rna'].obs[args.categorical_key].unique():
+                    for class_, thresh in [('enhancer', fimo_thresh_enhancer), 
+                                        ('promoter', fimo_thresh_promoter)]:
+
+                    
+                    
+                        loci_file = '/oak/stanford/groups/engreitz/Users/ymo/Tools/cNMF_benchmarking/cNMF_benchmarking_pipeline/Evaluation/Resources/scE2G_links/EnhancerPredictionsAllPutative.ForVariantOverlap.shrunk150bp_D{}_{}.tsv'.format(i, class_)
+                        motif_match_df, motif_count_df, motif_enrichment_df = compute_motif_enrichment(
+                            mdata, 
+                            prog_key='cNMF',
+                            data_key='rna',
+                            motif_file='/oak/stanford/groups/engreitz/Users/ymo/Tools/cNMF_benchmarking/cNMF_benchmarking_pipeline/Evaluation/Resources/hocomoco_meme.meme',
+                            seq_file='/oak/stanford/groups/engreitz/Users/ymo/Tools/cNMF_benchmarking/cNMF_benchmarking_pipeline/Evaluation/Resources/hg38.fa',
+                            loci_file=loci_file,
+                            window=1000,
+                            sig=thresh,
+                            eps=1e-4,
+                            n_top=2000,
+                            n_jobs=-1,
+                            inplace=False
+                        )
+
+                        motif_match_df.to_csv(os.path.join(args.out_dir, f'cNMF_{class_}_pearson_topn2000_{samp}_motif_match.txt'), sep='\t', index=False)
+                        motif_count_df.to_csv(os.path.join(args.out_dir, f'cNMF_{class_}_pearson_topn2000_{samp}_motif_count.txt'), sep='\t', index=False)
+                        motif_enrichment_df.to_csv(os.path.join(args.out_dir, f'cNMF_{class_}_pearson_topn2000_{samp}_motif_enrichment.txt'), sep='\t', index=False)
+
+            # Run explained variance
+            if args.Perform_explained_variance:
+                compute_explained_variance(cnmf_obj, X, k, output_folder = output_folder)
+
--- cNMF_benchmarking_pipeline/Evaluation/Slurm_Version/cNMF_evaluation_pipeline.sh
+++ cNMF_benchmarking_pipeline/Evaluation/Slurm_Version/cNMF_evaluation_pipeline.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # SLURM job configuration
-#SBATCH --job-name=batch_halsvar_cd_e7_eval           # Job name
-#SBATCH --output=/oak/stanford/groups/engreitz/Users/ymo/NMF_re-inplementing/Results/Consensus_across_methods/batch_halsvar_cd_e7/Eval/logs/%j.out      # Output file (%j = job ID)
-#SBATCH --error=/oak/stanford/groups/engreitz/Users/ymo/NMF_re-inplementing/Results/Consensus_across_methods/batch_halsvar_cd_e7/Eval/logs/%j.err       # Error file
-#SBATCH --partition=engreitz              # partition name
+#SBATCH --job-name=111025_D0_IGVF_10iter_torch_halsvar_batch_e7_v100s_test           # Job name
+#SBATCH --output=/oak/stanford/groups/engreitz/Users/ymo/cc-perturb-seq/Results/111025_D0_IGVF_10iter_torch_halsvar_batch_e7_v100s_test/Eval/logs/%j.out      # Output file (%j = job ID)
+#SBATCH --error=/oak/stanford/groups/engreitz/Users/ymo/cc-perturb-seq/Results/111025_D0_IGVF_10iter_torch_halsvar_batch_e7_v100s_test/Eval/logs/%j.err       # Error file
+#SBATCH --partition=engreitz            # partition name
 #SBATCH --time=05:00:00                 # Time limit 
 #SBATCH --nodes=1                       # Number of nodes
 #SBATCH --ntasks=1                      # Number of tasks
 #SBATCH --cpus-per-task=20              # CPUs per task
-#SBATCH --mem=80G                       # Memory per node
+#SBATCH --mem=96G                       # Memory per node
 
 # Email notifications
 #SBATCH --mail-type=BEGIN              # Send email when job starts
@@ -18,8 +18,8 @@
 #SBATCH --mail-user=ymo@stanford.edu   # the email address sent 
 
 # Define the cNMF case
-OUT_DIR="/oak/stanford/groups/engreitz/Users/ymo/NMF_re-inplementing/Results/Consensus_across_methods"
-RUN_NAME="batch_halsvar_cd_e7"
+OUT_DIR="/oak/stanford/groups/engreitz/Users/ymo/cc-perturb-seq/Results"
+RUN_NAME="111025_D0_IGVF_10iter_torch_halsvar_batch_e7_v100s_test"
 LOG_DIR="$OUT_DIR/$RUN_NAME"
 
 # Store start time
@@ -50,23 +50,27 @@ echo "Python path: $(which python)"
 # Run the Python script
 echo "Running Python script..."
 python3 /oak/stanford/groups/engreitz/Users/ymo/Tools/cNMF_benchmarking/cNMF_benchmarking_pipeline/Evaluation/Slurm_Version/cNMF_evaluation_pipeline.py \
-        --X_normalized "$LOG_DIR/cnmf_tmp/$RUN_NAME.norm_counts.h5ad" \
         --out_dir "$OUT_DIR" \
         --run_name "$RUN_NAME" \
-        --input_folder "$LOG_DIR" \
+        --X_normalized_path "$LOG_DIR/cnmf_tmp/$RUN_NAME.norm_counts.h5ad" \
         --Perform_explained_variance \
         --Perform_categorical \
         --Perform_perturbation \
         --Perform_geneset \
         --Perform_trait \
-        --assign_guide \
-        --dictionary_file_path "/oak/stanford/groups/engreitz/Users/ymo/Tools/cNMF_benchmarking/cNMF_benchmarking_pipeline/Evaluation/Resources/weissman_guides_with_coordinates.tsv" \
-        --assign_guide_key "cNMF_100"    \
-        #--mdata_guide "$LOG_DIR/adata/cNMF_30_2.0.h5mu" \
-        #any file that is h5mu with guide assignment in obms and uns is fine 
+        --data_key 'rna' \
+        --prog_key 'cNMF' \
+        --categorical_key 'batch' \
+        --organism 'human' \
+        --mdata_guide_path "/oak/stanford/groups/engreitz/Users/ymo/cc-perturb-seq/Data/IGVF_D0_example.h5mu" \
+        --guide_annotation_path "/oak/stanford/groups/engreitz/Users/ymo/cc-perturb-seq/Data/guide/guide_metadata_v43.tsv" \
+        --gwas_data_path '/oak/stanford/groups/engreitz/Users/ymo/Tools/cNMF_benchmarking/cNMF_benchmarking_pipeline/Evaluation/Resources/OpenTargets_L2G_Filtered.csv.gz' \
+        --reference_gtf_path "/oak/stanford/groups/engreitz/Users/opushkar/genome/IGVFFI9573KOZR.gtf.gz" 
         #--Perform_motif \
+        #--K \
+        #--sel_thresh \
 
-        
+
 
 
 # Calculate and print elapsed time at the end
